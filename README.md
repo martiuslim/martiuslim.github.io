01e354f53c7ce7727a6fdbe6cbdc44d4836409a6
martiuslim <martiuslim@outlook.com>
2017-12-03 01:43:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# [martiuslim.io](http://martiuslim.io)
+This repository houses the source code for my personal site.
---
+++ js/scripts.js
@@ -0,0 +1,139 @@
+/*!
+    Title: Dev Portfolio Template
+    Version: 1.2.1
+    Last Change: 08/27/2017
+    Author: Ryan Fitzgerald
+    Repo: https://github.com/RyanFitzgerald/devportfolio-template
+    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
+
+    Description: This file contains all the scripts associated with the single-page
+    portfolio website.
+*/
+
+(function($) {
+
+    // Remove no-js class
+    $('html').removeClass('no-js');
+
+    // Animate to section when nav is clicked
+    $('header a').click(function(e) {
+
+        // Treat as normal link if no-scroll class
+        if ($(this).hasClass('no-scroll')) return;
+
+        e.preventDefault();
+        var heading = $(this).attr('href');
+        var scrollDistance = $(heading).offset().top;
+
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 2.5);
+
+        // Hide the menu once clicked if mobile
+        if ($('header').hasClass('active')) {
+            $('header, body').removeClass('active');
+        }
+    });
+
+    // Scroll to top
+    $('#to-top, #to-top-corner').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // Scroll to first element
+    $('#lead-down span').click(function() {
+        var scrollDistance = $('#lead').next().offset().top;
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, 500);
+    });
+
+    // Create timeline
+    $('#experience-timeline').each(function() {
+
+        $this = $(this); // Store reference to this
+        $userContent = $this.children('div'); // user content
+
+        // Create each timeline block
+        $userContent.each(function() {
+            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+        });
+
+        // Add icons to each block
+        $this.find('.vtimeline-point').each(function() {
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+        });
+
+        // Add dates to the timeline if exists
+        $this.find('.vtimeline-content').each(function() {
+            var date = $(this).data('date');
+            if (date) { // Prepend if exists
+                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
+            }
+        });
+
+    });
+
+    // Edit projects
+    $('#projects-list').each(function() {
+
+        $this = $(this);
+        $project = $this.children('div');
+
+        $project.each(function() {
+            $projectContainer = $(this).children('div');
+
+            if ($(this).hasClass('no-image')) {
+                $(this).prepend('<div class="col-md-2"></div>');
+                $projectContainer.addClass('col-md-8');
+            }
+        });
+
+    });
+
+    // Open mobile menu
+    $('#mobile-menu-open').click(function() {
+        $('header, body').addClass('active');
+    });
+
+    // Close mobile menu
+    $('#mobile-menu-close').click(function() {
+        $('header, body').removeClass('active');
+    });
+
+    // Load additional projects
+    $('#view-more-projects').click(function(e){
+        e.preventDefault();
+        $(this).fadeOut(300, function() {
+            $('#more-projects').fadeIn(300);
+        });
+    });
+
+})(jQuery);
+
+(function($) {
+    "use strict";
+
+    $('[data-typer-targets]').typer();
+    $.typer.options.clearOnHighlight=false;
+
+})(jQuery);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("to-top-corner").style.display = "block";
+    } else {
+        document.getElementById("to-top-corner").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
